fa236b836927ea369818e003d10970292d59d2e7
vadimGridasov <vadim228849675@outlook.com>
2024-11-20 22:47:52 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+
+
+firs_name='Python,'
+second_name='12,'
+home=12
+third_name='1.5,'
+time=1.5
+print(('Курс:'+ firs_name),('всего задач:' + second_name),('времени затрачено:'+ third_name),('среднее время выполнения:'+ (str(time/home))+ 'часа.'))
