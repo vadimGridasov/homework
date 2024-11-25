aeb68c0a42a0d24df4d5fce9234a4d2b244dc14d
Vadim <vadim228849675@outlook.com>
2024-11-25 21:57:33 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,21 @@
+my_dict={'Vadim' : 2002, 'Oleg' : 2001, 'Lexa' : 2001, 'Pavel': 1999}
+print(my_dict)
+print(my_dict['Oleg'])
+print(my_dict.get('Vasya'))
+my_dict.update({'Suleiman': 1494, 'Sasha':1789})
+print(my_dict)
+a=my_dict.pop('Oleg')
+print(my_dict)
+print(a)
+
+
+
+my_set={1,2,3,4,5,3,2,1,'Stop',(1,2,3), False}
+print(my_set)
+print(my_set.add(8))
+print(my_set.add(9))
+print(my_set.discard(3))
+print(my_set)
+
+
+
