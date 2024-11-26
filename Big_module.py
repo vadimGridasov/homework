fc76055b18bc42220824cfa23e132f49903990fb
Vadim <vadim228849675@outlook.com>
2024-11-26 23:11:16 +0300

    Add files via upload

---
+++ Big_module.py
@@ -0,0 +1,17 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students2=sorted(students)
+print(students2)
+a=[(5+ 3+ 3+ 5+ 4)/5]
+print(a)
+b=[(2+ 2+ 2+ 3)/4]
+print(b)
+c=[(4+ 5+ 5+ 2)/4]
+print(c)
+d=[(4+ 4+ 3)/3]
+print(d)
+e=[(5+ 5+ 5+ 4+ 5)/5]
+print(e)
+age_grades={'Aaron':sum(a), 'Bilbo':sum(b), 'Johnny':sum(c), 'Khendrik':sum(d), 'Steve':sum(e)}
+print(age_grades)
+
